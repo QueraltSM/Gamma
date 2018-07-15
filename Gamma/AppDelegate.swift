0a2fa2f241bc58c5651e1f70bd4d0e3435ca18c6
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-07-15 10:57:06 +0100

    Save and retrieve data with Firebase

--- Gamma/AppDelegate.swift
+++ Gamma/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    override init() {
+        FIRApp.configure()
+        FIRDatabase.database().persistenceEnabled = true
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- Gamma/ViewController.swift
+++ Gamma/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import Firebase
 
-class ViewController: UIViewController {
+struct postStruct {
+    let title : String!
+    let message : String!
+}
 
+class ViewController: UITableViewController {
+    
+    var posts = [postStruct]()
+    let ref = FIRDatabase.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Retrieve data
+        ref.child("Posts").queryOrderedByKey().observe(.childAdded, with: {
+            snapshot in
+            let value = snapshot.value as? NSDictionary
+            let title = value!["title"] as? String
+            let message = value!["message"] as? String
+            
+            self.posts.insert(postStruct(title:title,message:message), at: 0)
+            self.tableView.reloadData()
+        })
+        post()
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    func post() {
+        // Save data
+        let title="Title"
+        let message="Message"
+        
+        let post : [String : AnyObject] = ["title" : title as AnyObject,
+                                           "message" : message as AnyObject]
+        ref.child("Posts").childByAutoId().setValue(post)
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        let label1 = cell?.viewWithTag(1) as! UILabel
+        label1.text = posts[indexPath.row].title
+        let label2 = cell?.viewWithTag(2) as! UILabel
+        label2.text = posts[indexPath.row].message
+        return cell!
+    }
 }
+
 
